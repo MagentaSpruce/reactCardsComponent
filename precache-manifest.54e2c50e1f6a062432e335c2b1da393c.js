f9e2badf62298cc445899036f263678efd2e3142
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-02 18:20:14 -0400

    Updates

---
+++ precache-manifest.54e2c50e1f6a062432e335c2b1da393c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4167031360eb5a48c413148f29de53",
+    "url": "/reactToursApp/index.html"
+  },
+  {
+    "revision": "8d7a82e5a09c62aaf7c9",
+    "url": "/reactToursApp/static/css/main.c3e53c0d.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/reactToursApp/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactToursApp/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d7a82e5a09c62aaf7c9",
+    "url": "/reactToursApp/static/js/main.841fcb87.chunk.js"
+  },
+  {
+    "revision": "9edd2e20c45523b86107",
+    "url": "/reactToursApp/static/js/runtime-main.8b178090.js"
+  }
+]);
